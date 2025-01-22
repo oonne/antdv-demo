71e2acbfb01ed43b13cd3617d05d13b0ac8e28f7
JAY <JAY@oonne.com>
2025-01-22 17:58:24 +0800

    chore: 初始化脚手架

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { CodeInspectorPlugin } from 'code-inspector-plugin';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+
+// https://cn.vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    CodeInspectorPlugin({
+      bundler: 'vite',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8100,
+  },
+  build: {
+    assetsInlineLimit: 0,
+  },
+});
